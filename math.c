fe309ee9fc350101570bb1a0e7d3c5133cc3e197
Ksena <oksanaivaniv45@gmail.com>
2024-02-26 13:56:35 +0300

    Changes into clone repository.

--- math.c
+++ math.c
@@ -9,6 +9,10 @@ namespace Maths
             double radius = 50;
 	    double area = Math.PI * Math.Pow(radius, 2);
 	    Console.WriteLine($"Площадь круга с радиусом {radius} равна {Math.Round(area,2)}"); 
+	    double a = 5;
+	    double b = 8;
+	    double res = a*b;
+	    Console.WriteLine($"Результат произведения {a} на {b} равен {res}")
         }
     }
 }
